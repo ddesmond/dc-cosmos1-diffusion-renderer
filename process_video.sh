ef53ca12c8191332cbae74bdd660af5e050fb885
Aleks <crni.aki@gmail.com>
2025-09-07 07:07:00 +0200

    init

--- process_video.sh
+++ process_video.sh
@@ -1,7 +1,10 @@
 CUDA_HOME=$CONDA_PREFIX PYTHONPATH=$(pwd) python cosmos_predict1/diffusion/inference/inference_inverse_renderer.py \
     --checkpoint_dir checkpoints --diffusion_transformer_dir Diffusion_Renderer_Inverse_Cosmos_7B \
-    --dataset_path=asset/examples/video_frames_examples/ --num_video_frames 57 --group_mode folder \
+    --batch_input_path=asset/examples/video_frames_examples/ \
+    --fps 25 \
+    --num_video_frames 57 --group_mode folder \
     --overlap_n_frames 32 \
+    --chunk_mode all \
     --normalize_normal 1 \
     --video_save_folder=asset/example_results/video_delighting/ \
     --offload_diffusion_transformer --offload_tokenizer
